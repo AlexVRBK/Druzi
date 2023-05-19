f12bc6908a0d5a2795326f87d9b43fe5780e4da0
Alex VRBK <vvvorobok@gmail.com>
2023-05-19 11:38:57 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.swiper-button-right',
+    prevEl: '.swiper-button-left',
+  },
+  initialSlide: 0, // Початковий слайд (індексується з 0)
+});
+
+
+
+
+
