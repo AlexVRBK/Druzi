41f9a26c1488fa51a843860b61ab681b8f38ce4c
Alex VRBK <vvvorobok@gmail.com>
2023-05-21 23:10:57 +0300

    plus

--- js/main.js
+++ js/main.js
@@ -37,6 +37,18 @@ btns.forEach((btn, index) => {
   });
 });
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+
+
 
 
 
